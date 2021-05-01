361350db8a9565892fb050a44dfc18a43ba5893f
everettjf <everettjf@live.com>
2021-05-01 23:38:25 +0800

    parse arguments

---
+++ src/cli.rs
@@ -0,0 +1,15 @@
+use structopt::StructOpt;
+use std::path::PathBuf;
+
+#[derive(Debug, StructOpt)]
+#[structopt(
+  name = "dirsum",
+  about = "Get formatted summary for specified directory.",
+)]
+pub struct Opt {
+  #[structopt(short, long, parse(from_os_str), help("Path to a directory"))]
+  pub path: PathBuf,
+
+  #[structopt(short, long, help("Print with json format"))]
+  pub json: Option<bool>,
+}
